e805a02fbf4f3db5e6a1cadb0004e46b4d10e311
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-03 22:54:27 +0530

    Settings up MaterVC

--- Splitter/Images+Names+Colors.swift
+++ Splitter/Images+Names+Colors.swift
@@ -22,4 +22,6 @@ let imageArray = [camera,city,buffalo,bouqet,stand,urban]
 //MARK: - Names
 var namesArray = ["CAMERA", "CITY", "BUFFALO", "BOUQET", "STAND", "URBAN"]
 
+//MARKS: - Color
+var colorArray = [#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1) , #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1) ,#colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1) ,#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1) ,#colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1) ,#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1) ]
 
