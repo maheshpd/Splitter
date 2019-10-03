9a5d53e64ac83d84cd3d0e4aa61eb3cb1212b2c5
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-10-03 22:38:25 +0530

    Creating Project and Dummy Data

---
+++ Splitter/Images+Names+Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Images+Names+Colors.swift
+//  Splitter
+//
+//  Created by Mahesh Prasad on 03/10/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+//MARK:- Images
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera,city,buffalo,bouqet,stand,urban]
+
+//MARK: - Names
+var namesArray = ["CAMERA", "CITY", "BUFFALO", "BOUQET", "STAND", "URBAN"]
+
+
